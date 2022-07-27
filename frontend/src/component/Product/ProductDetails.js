9fc0a5b69e885612d432c3a9c0c6f04baa9e77f1
bhima2001 <bhimashankar.d19@iiits.in>
2022-07-27 21:57:44 +0530

    error in product details page due to ratings

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -34,7 +34,7 @@ const ProductDetails = () => {
     color:'rgba(20,20,20,0.1)',
     activeColor:'tomato',
     size:window.innerWidth<600?20:25,
-    value:product.rating,
+    value:product.ratings,
     isHalf:true,
 }
 const addToCartHandler =()=>{
